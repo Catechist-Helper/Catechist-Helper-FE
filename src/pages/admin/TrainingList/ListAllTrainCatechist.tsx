2054f5c41e11a5655e042794736df584e690b958
Tinatem <thanhtam71530@gmail.com>
2024-12-16 02:26:28 +0700

    update delete status

--- src/pages/admin/TrainingList/ListAllTrainCatechist.tsx
+++ src/pages/admin/TrainingList/ListAllTrainCatechist.tsx
@@ -340,7 +340,6 @@ const ListAllTrainCatechist: React.FC = () => {
                             const keyNum = parseInt(key, 10);
                             const isDisabled =
                               // 1. Khi Training chưa bắt đầu (status = 0)
-                              (trainingStatus === 0 && keyNum === 2) ||
                               (trainingStatus === 0 && (keyNum === 1 || keyNum === 2)) ||
                               // 2. Khi Training đang đào tạo (status = 1)
                               (trainingStatus === 1 &&
